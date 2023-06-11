0f39cba1848b29410454734a328768cb617aef1a
arafataft <arafatcd@gmail.com>
2023-06-11 22:24:29 +0600

    remove close

--- index.js
+++ index.js
@@ -5,10 +5,28 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-
 app.use(cors());
 app.use(express.json());
 
+// verify jwt
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' })
+  }
+  // bearer token
+  const token = authorization.split(' ')[1]
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded
+    next()
+  })
+}
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fwntuaw.mongodb.net/?retryWrites=true&w=majority`;
@@ -24,31 +42,45 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
 
-    const Classes=client.db('survivalDB').collection('classes')
+    const ClassesCollection = client.db('survivalDB').collection('Classes')
+    const UserCollection = client.db('survivalDB').collection('users')
 
+    // user api 
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await UserCollection.insertOne(user);
+      console.log(result);
+      res.send(result);
+    })
 
-
-
-
+    //jwt token
+    app.post('/jwt', (req, res) => {
+      const email = req.body
+      console.log(email)
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h',
+      })
+      console.log(token);
+      res.send(token)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+  } catch (error) {
+    console.error('Failed to connect to MongoDB', error);
   }
 }
+
 run().catch(console.dir);
 
+app.get('/', (req, res) => {
+  res.send('server is running');
+});
 
-app.get('/',(req,res)=>{
-    res.send('server is running')
-})
-
-app.listen(port,()=>{
-    console.log(`Server is on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is on port ${port}`);
+});
